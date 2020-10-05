3e7512cc3375c8515161711a15312770edbc4110
MohamedAhmed315 <mohamedahmed315@hotmail.com>
2020-10-05 17:38:57 +0200

    update

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,17 +1,10 @@
-window.onscroll = function() {scrollFunction()};
 
-var navbar = document.getElementById("top-nav");
-var sticky = navbar.offsetTop;
+//portfolio image display
 
-function scrollFunction() {
-  if (window.pageYOffset >= sticky) {
-    document.getElementById("top-nav").style.padding = "5px";
-    navbar.classList.add("sticky");
-    // document.getElementById("top-nav").style.fontSize = "25px";
-  } else {
-    document.getElementById("top-nav").style.padding = "15px";
-    // document.getElementById("top-nav").style.fontSize = "35px";
-  }
-}
-
+$(function() {  
+  $('.pop').on('click', function() {
+      $('.imagepreview').attr('src', $(this).find('img').attr('src'));
+      $('#imagemodal').modal('show');   
+  });		
+});
 
